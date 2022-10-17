21c925c8743b937da19dba808ccd08a4d09a530a
Joe <JosephGlavan@users.noreply.github.com>
2022-10-17 16:37:13 -0400

    Add files via upload
    Analysis script used for evaluating pilot condition stimuli

---
+++ AGRT Quick Accuracy Check.R
@@ -0,0 +1,12 @@
+#### FILE PATH ####
+myfile <- "/Users/joeglavan/Documents/Research Projects/Adaptive GRT/Human Study/Experiment Code/data/TEST_2021_Nov_12_1623.txt"
+###################
+
+mydata <- read.table(myfile, header=T, sep="\t", stringsAsFactors = F)
+mydata <- subset(mydata, Block=="MAIN")
+print("Accuracy by Stimulus",quote=F)
+for (s in 0:3) {
+  print(paste("Stimulus ",s," (",switch(s+1, "LL","LH","HL","HH"),"): ", round(mean(mydata$Correct[mydata$Stimulus==s]), 4), sep=''), quote=F)
+}
+print("Overall Accuracy (targeting 75%)", quote=F)
+print(round(mean(sapply(0:3, function (x) mean(mydata$Correct[mydata$Stimulus==x]))), 4), quote=F)
