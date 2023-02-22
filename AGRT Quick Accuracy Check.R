bc206b242feb91d7cf28cd2f9f92729f80f3f0e6
Joe <JosephGlavan@users.noreply.github.com>
2023-02-21 19:02:54 -0500

    Update AGRT Quick Accuracy Check.R
    Added Chi Square test for random responding

--- AGRT Quick Accuracy Check.R
+++ AGRT Quick Accuracy Check.R
@@ -10,3 +10,5 @@ for (s in 0:3) {
 }
 print("Overall Accuracy (targeting 75%)", quote=F)
 print(round(mean(sapply(0:3, function (x) mean(mydata$Correct[mydata$Stimulus==x]))), 4), quote=F)
+
+if (chisq.test(table(mydata$Stimulus, mydata$Response))$p.value < .05) print("Responses are statistically non-random. ACCEPT SUBJECT.", quote=F) else print("Responses are statistically random. REJECT SUBJECT.", quote=F)
